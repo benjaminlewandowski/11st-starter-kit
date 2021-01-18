c95ea374998a16a196d4265bffd2d96b1e514448
benjaminlewandowski <benjamin.lewandowski.de@gmail.com>
2021-01-18 22:29:32 +0100

    fix(packages): updated

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,30 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = {
+        plugins: [
+            require('postcss-short'),
+            require('postcss-sorting'),
+            require('postcss-utilities'),
+            require('tailwindcss'),
+            require('postcss-nested'),
+            require('postcss-preset-env')({ stage: 1 }),
+            require('cssnano')({
+                preset: 'default',
+            }),
+            require('@fullhuman/postcss-purgecss')({
+                content: ['./src/**/*.njk', './src/**/*.js', './src/**/*.md'],
+            }),
+            require('postcss-sort-media-queries'),
+        ],
+    };
+} else {
+    module.exports = {
+        plugins: [
+            require('postcss-short'),
+            require('postcss-sorting'),
+            require('postcss-utilities'),
+            require('tailwindcss'),
+            require('postcss-nested'),
+            require('postcss-preset-env')({ stage: 1 }),
+        ],
+    };
+}
