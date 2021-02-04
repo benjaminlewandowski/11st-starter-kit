e9d8cbbd09a4987a92c264875bf46a3aa4d1a8c6
benjaminlewandowski <benjamin.lewandowski.de@gmail.com>
2021-02-04 21:09:09 +0100

    postcss fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     plugins: [
-        require('postcss-short'),
         require('tailwindcss'),
         require('postcss-preset-env')({ stage: 1 }),
         ...(process.env.NODE_ENV === 'production'
             ? [
+                  require('@fullhuman/postcss-purgecss'),
                   require('postcss-sorting'),
                   require('postcss-sort-media-queries'),
                   require(`cssnano`)({
